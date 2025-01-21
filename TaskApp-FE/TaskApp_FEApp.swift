bff99d4c5fd7f1c88e0a3e8a42791e5551fbbdce
Ogunriwa <ibrahimaro4@gmail.com>
2025-01-21 15:22:25 -0600

    Initial Commit

---
+++ TaskApp-FE/TaskApp_FEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp_FEApp.swift
+//  TaskApp-FE
+//
+//  Created by Ibrahim Arogundade on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp_FEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskApp-FETests/TaskApp_FETests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp_FETests.swift
+//  TaskApp-FETests
+//
+//  Created by Ibrahim Arogundade on 1/21/25.
+//
+
+import Testing
+@testable import TaskApp_FE
+
+struct TaskApp_FETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
